57e9d5afa97dd21a84a4404d618fef7c8b584eb0
Alexander Atallah <alexander.atallah@gmail.com>
2019-07-16 02:04:35 +0800

    small change to Wyvern over tradeable

--- contracts/Creature.sol
+++ contracts/Creature.sol
@@ -1,14 +1,14 @@
 pragma solidity ^0.5.0;
 
-import "./TradeableERC721Token.sol";
+import "./WyvernERC721Token.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 /**
  * @title Creature
  * Creature - a contract for my non-fungible creatures.
  */
-contract Creature is TradeableERC721Token {
-  constructor(address _proxyRegistryAddress) TradeableERC721Token("Creature", "OSC", _proxyRegistryAddress) public {  }
+contract Creature is WyvernERC721Token {
+  constructor(address _proxyRegistryAddress) WyvernERC721Token("Creature", "OSC", _proxyRegistryAddress) public {  }
 
   function baseTokenURI() public view returns (string memory) {
     return "https://opensea-creatures-api.herokuapp.com/api/creature/";
